5aa5d22bdd07a3b10514baf8d0bdda8450fcc4da
wstoccob <alinur.seysekov@gmail.com>
2024-01-09 18:38:32 +0600

    Add Sprite class

---
+++ Megalith/Megalith/Sprite.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Megalith;
+
+public class Sprite
+{
+    protected Texture2D _texture;
+    public Vector2 _position { get; set; }
+
+    public Sprite(Texture2D texture, Vector2 position)
+    {
+        _texture = texture;
+        _position = position;
+    }
+
+    public virtual void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_texture, _position, Color.White);
+    }
+}
